cdc2ce07a11d065f0ca81b4194e1f74a6868422d
Aramide <aramideotenaike@gmail.com>
2026-03-30 00:09:08 +0100

    refactored

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,8 @@
 import api from "./axiosInstance";
 
-export const register = (data) => api.post("/api/auth/register", data);
+export const register = (data) => api.post("/auth/register", data);
 
 export const login = (email, password) =>
-  api.post("/api/auth/login", { email, password });
+  api.post("/auth/login", { email, password });
 
 export const deleteAccount = () => api.delete("/auth/account");
